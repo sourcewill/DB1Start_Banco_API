57fa86d1ee39cb8d77d4a95041f472642af12970
William Rodrigues <email.williamrodrigues@gmail.com>
2019-11-25 16:56:53 -0300

    Iniciando projeto.

---
+++ src/main/java/com/db1start/cidades/CidadesApplication.java
@@ -0,0 +1,13 @@
+package com.db1start.cidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db1start/cidades/CidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db1start.cidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
