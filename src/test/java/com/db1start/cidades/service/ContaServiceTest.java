c7da739c4796b20d670231f237437d2a4ad83a5e
William Rodrigues <email.williamrodrigues@gmail.com>
2019-11-26 21:12:21 -0300

    Implementando testes unitarios.

---
+++ src/test/java/com/db1start/cidades/service/ContaServiceTest.java
@@ -0,0 +1,55 @@
+package com.db1start.cidades.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.db1start.cidades.domain.entity.Agencia;
+import com.db1start.cidades.domain.entity.Cidade;
+import com.db1start.cidades.domain.entity.Cliente;
+import com.db1start.cidades.domain.entity.Conta;
+import com.db1start.cidades.domain.entity.Estado;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContaServiceTest {
+
+	@Autowired
+	private ContaService contaService;
+
+	@Autowired
+	private AgenciaService agenciaService;
+
+	@Autowired
+	private CidadeService cidadeService;
+
+	@Autowired
+	private EstadoService estadoService;
+
+	@Autowired
+	private ClienteService clienteService;
+	
+	private void clean() {
+		contaService.limpar();
+		agenciaService.limpar();
+		cidadeService.limpar();
+		estadoService.limpar();
+	}
+
+	@Test
+	public void deveCriarNovaConta() {
+		Estado estado = estadoService.criar("Parana");
+		Cidade cidade = cidadeService.criar("Maringa", estado);
+		Agencia agencia = agenciaService.criar("123", cidade, "123");
+		Cliente cliente = clienteService.criar("William", "123");
+		Conta conta = contaService.criar(cliente, agencia);
+		
+		assertNotNull(conta);
+		clean();
+	}
+
+}
