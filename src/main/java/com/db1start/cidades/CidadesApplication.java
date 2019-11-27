26f496d776a3e586e8537ff5f1942238ea7818dd
William Rodrigues <email.williamrodrigues@gmail.com>
2019-11-27 16:03:09 -0300

    Metodos para buscar contas, ativas ou inativas.

--- src/main/java/com/db1start/cidades/CidadesApplication.java
+++ src/main/java/com/db1start/cidades/CidadesApplication.java
@@ -1,13 +1,48 @@
 package com.db1start.cidades;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.db1start.cidades.domain.entity.Agencia;
+import com.db1start.cidades.domain.entity.Cidade;
+import com.db1start.cidades.domain.entity.Cliente;
+import com.db1start.cidades.domain.entity.Conta;
+import com.db1start.cidades.domain.entity.Estado;
+import com.db1start.cidades.service.AgenciaService;
+import com.db1start.cidades.service.CidadeService;
+import com.db1start.cidades.service.ClienteService;
+import com.db1start.cidades.service.ContaService;
+import com.db1start.cidades.service.EstadoService;
+
 @SpringBootApplication
 public class CidadesApplication {
+	
+	@Autowired
+	private static AgenciaService agenciaService;
+
+	@Autowired
+	private static CidadeService cidadeService;
+
+	@Autowired
+	private static EstadoService estadoService;
+
+	@Autowired
+	private static ClienteService clienteService;
+	
+	@Autowired
+	private static ContaService contaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CidadesApplication.class, args);
+		System.out.println("Iniciando...");
+		
+		Estado estado = estadoService.criar("Parana");
+		Cidade cidade = cidadeService.criar("Maringa", estado);
+		Agencia agencia = agenciaService.criar("123", cidade, "123");
+		Cliente cliente = clienteService.criar("William", "123");
+		Conta conta = contaService.criar(cliente, agencia);
+
 	}
 
 }
--- src/main/java/com/db1start/cidades/service/ContaService.java
+++ src/main/java/com/db1start/cidades/service/ContaService.java
@@ -1,11 +1,15 @@
 package com.db1start.cidades.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.db1start.cidades.domain.entity.Agencia;
 import com.db1start.cidades.domain.entity.Cliente;
 import com.db1start.cidades.domain.entity.Conta;
+import com.db1start.cidades.domain.entity.StatusConta;
 import com.db1start.cidades.repository.ContaRepository;
 
 @Service
@@ -31,5 +35,31 @@ public class ContaService {
 		return contaRepository.findById(id).orElseThrow(
 				() -> new RuntimeException("Conta com id " + id + " nao encontrada no banco de dados."));
 	}
+	
+	public List<Conta> buscaTodasContas(){
+		return contaRepository.findAll();
+	}
+	
+	public List<Conta> buscaTodasContasAtivas(){
+		List<Conta> ativas = new ArrayList<>();
+		for(Conta conta : contaRepository.findAll()) {
+			if(conta.getStatus() == StatusConta.ATIVA) {
+				ativas.add(conta);
+			}
+		}
+		return ativas;
+	}
+	
+	public List<Conta> buscaTodasContasInativas(){
+		List<Conta> inativas = new ArrayList<>();
+		for(Conta conta : buscaTodasContas()) {
+			if(conta.getStatus() == StatusConta.INATIVA) {
+				inativas.add(conta);
+			}
+		}
+		return inativas;
+	}
+	
+	
 
 }
