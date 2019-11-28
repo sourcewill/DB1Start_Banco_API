228aba6ce112be8bda633464712fcf051a1ac51e
William Rodrigues <email.williamrodrigues@gmail.com>
2019-11-28 16:41:34 -0300

    Refatorando classe Agencia.

--- src/main/java/com/db1start/cidades/domain/entity/Agencia.java
+++ src/main/java/com/db1start/cidades/domain/entity/Agencia.java
@@ -1,18 +1,12 @@
 package com.db1start.cidades.domain.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -32,9 +26,6 @@ public class Agencia {
 
 	@Column(name = "numeroBanco")
 	private String numeroBanco;
-	
-	@OneToMany(mappedBy = "agencia", cascade = CascadeType.ALL, fetch = FetchType.LAZY)	
-	private List<Conta> contas;
 
 	public Agencia(String numeroAgencia, Cidade cidade, String numeroBanco) {
 
@@ -51,7 +42,6 @@ public class Agencia {
 		this.numeroAgencia = numeroAgencia;
 		this.cidade = cidade;
 		this.numeroBanco = numeroBanco;
-		this.contas = new ArrayList<>();
 	}
 
 	public Agencia() {
