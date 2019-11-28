673b0781277aece6b8d5711503953d3c3b0dc0bb
William Rodrigues <email.williamrodrigues@gmail.com>
2019-11-28 18:15:58 -0300

    Implementando metodos no Controller de Estado.

--- src/main/java/com/db1start/cidades/controller/EstadoController.java
+++ src/main/java/com/db1start/cidades/controller/EstadoController.java
@@ -1,14 +1,47 @@
 package com.db1start.cidades.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.db1start.cidades.domain.entity.Estado;
+import com.db1start.cidades.service.EstadoService;
 
 @RestController
 public class EstadoController {
-	
+
+	@Autowired
+	private EstadoService estadoService;
+
 	@GetMapping("/estados")
-	public String buscarTodosEstados() {
-		return "Meus Estados";
+	public List<Estado> buscarTodosEstados() {
+		return estadoService.buscarTodosEstados();
+	}
+
+	@GetMapping("/estado/{id}")
+	public Estado buscarEstadoPorId(@PathVariable(value = "id") Long id) {
+		return estadoService.buscarEstadoPorId(id);
+	}
+
+	@PostMapping("/estado")
+	public Estado criarEstado(@RequestBody String nome) {
+		return estadoService.criar(nome);
+	}
+
+	@DeleteMapping("/estado/{id}")
+	public void apagarEstadoPorId(@PathVariable(value = "id") Long id) {
+		estadoService.apagarEstadoPorId(id);
+	}
+
+	@DeleteMapping("/estados")
+	public void apagarTodosEstados() {
+		estadoService.limpar();
 	}
 
 }
